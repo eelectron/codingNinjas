9ddc8c8888507bb57fca778a98d2325051890775
Prashant Singh <prashantexploring@gmail.com>
2020-11-06 19:23:27 +0530

    innocent

--- numberTheory/Innocent.java
+++ numberTheory/Innocent.java
@@ -4,21 +4,41 @@ import java.util.Scanner;
 
 public class Innocent {
 	long MOD = 1000000007;
+	int SIZE = 1000000;
+	long[] fac;
+	Exponent exp;
 	public Innocent() {
-		
+		exp = new Exponent();
+		fac = new long[SIZE + 1];
+		fac[0] = 1;
+		for(int i = 1; i < fac.length; i++) {
+			fac[i] = (fac[i - 1] % MOD * i % MOD) % MOD;
+		}
 	}
+	
 	/*
 	 * ans = C(n, k) * 2 ^k 
 	 * ans = ans % MOD*/
 	public long ways(long n, long k) {
 		// 2^k % m
+		long a = exp.modExpIt(2, k, MOD);
 		
+				
 		// n! % m
+		long b = fac[(int)n];
 		
 		//((k! % m)^(m - 2)) % m
+		long c = fac[(int)k];
+		c = exp.modExpIt(c, MOD - 2, MOD);
 		
 		// ((n - k)! % m)^(m - 2) % m
-		return 0;
+		long d = fac[(int)(n - k)];
+		d = exp.modExpIt(d, MOD - 2, MOD);
+		
+		long ans = (a * b) % MOD;
+		ans = (ans * c) % MOD;
+		ans = (ans * d) % MOD;
+		return ans;
 	}
 	
 	public static void main(String[] args) {
