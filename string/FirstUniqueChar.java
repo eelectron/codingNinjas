14f91abc4f5b09cf4e1d6ba61951be179f3f6d8b
Prashant Singh <psonlinux@gmail.com>
2021-05-13 09:37:10 +0530

    first unique char

--- string/FirstUniqueChar.java
+++ string/FirstUniqueChar.java
@@ -4,87 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 /*
- * https://leetcode.com/problems/first-unique-character-in-a-string/
+ * https://leetcode.com/submissions/detail/492263774/
  * 
  * The approach for this question 
  * https://sites.google.com/view/leetcodepractice/firstuniquechar
  * */
+
 public class FirstUniqueChar {
     public int firstUniqChar(String s) {
         int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
         
-        FirstNonUnique fu = new FirstNonUnique();
+        char ch;
         for(int i = 0; i < n; i++){
-            fu.insert(s.charAt(i), i);
-            //System.out.println(fu.getFirstNonUnique());
+            ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        return fu.getFirstNonUnique();
-    }
-}
-
-class Node{
-    int ch, id;
-    Node next, prev;
-    Node(int ch, int id){
-        this.id = id;
-        this.ch = ch;
-        this.prev = null;
-        this.next = null;
-    }
-
-    Node(){
-        this.id = -1;
-        this.ch = -1;
-        this.prev = null;
-        this.next = null;
-    }
-}
-
-class FirstNonUnique{
-    Map<Integer, Node> seen;
-    Node head, tail;
-    
-    public FirstNonUnique(){
-        seen = new HashMap<>();
         
-        head = new Node();
-        tail = new Node();
-        head.next = tail;
-        tail.prev = head;
-    }
-    
-    public void insert(int ch, int id){
-        if(seen.containsKey(ch) == false){
-            Node node = addNode(ch, id);
-            seen.put(ch, node);
-        }
-        else{
-            Node remNode = seen.get(ch);
-            if(remNode != null){
-                removeNode(remNode);
-                seen.put(ch, null);
+        for(int i = 0; i < n; i++){
+            if(map.get(s.charAt(i)) == 1){
+                return i;
             }
         }
+        return -1;
     }
-    
-    public int getFirstNonUnique(){
-        if(head.next == tail){
-            return -1;
-        }
-        return head.next.id;
-    }
-    
-    private Node addNode(int ch, int id){
-        Node nn = new Node(ch, id);
-        nn.next = tail;
-        nn.prev = tail.prev;
-        nn.prev.next = nn;
-        nn.next.prev = nn;
-        return nn;
-    }
-    
-    private void removeNode(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    } 
 }
