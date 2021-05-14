fbfba5cfe4e03c69d640bdf5805da8fface035f8
Prashant Singh <psonlinux@gmail.com>
2021-05-14 13:00:58 +0530

    largest histogram

---
+++ array/LargestHistogram.java
@@ -0,0 +1,48 @@
+package array;
+
+public class LargestHistogram {
+    /*
+    T : O(n)
+    S : O(n)
+    */
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0){
+            return 0;
+        }
+        
+        int n = heights.length;
+        if(n == 1){
+            return heights[0];
+        }
+        
+        // find index of first bar in left side whose height is less than current bar
+        int[] leftMinBar = new int[n];
+        leftMinBar[0] = -1;
+        for(int i = 1; i < n; i++){
+            int k = i - 1;
+            while(k >= 0 && heights[k] >= heights[i]){
+                k = leftMinBar[k];
+            }
+            leftMinBar[i] = k;
+        }
+        
+        // find index of first bar in right side whose height is less than current bar
+        int[] rightMinBar = new int[n];
+        rightMinBar[n - 1] = n;
+        for(int i = n - 2; i >= 0; i--){
+            int k = i + 1;
+            while(k < n && heights[k] >= heights[i]){
+                k = rightMinBar[k];
+            }
+            rightMinBar[i] = k;
+        }
+        
+        // consider largest rectangle formed by each bar
+        int maxArea = 0, curArea = 0;
+        for(int i = 0; i < n; i++){
+            curArea = heights[i] * (rightMinBar[i] - leftMinBar[i] - 1);
+            maxArea = Math.max(curArea, maxArea);
+        }
+        return maxArea;
+    }
+}
