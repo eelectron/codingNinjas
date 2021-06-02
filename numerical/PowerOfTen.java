aa9a28aa1d7d949e7fec697254b3fac0f2d8e477
Prashant Singh <psonlinux@gmail.com>
2021-06-02 18:08:13 +0530

    power of ten

---
+++ numerical/PowerOfTen.java
@@ -0,0 +1,60 @@
+package numerical;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/*
+ * Reference : https://leetcode.com/problems/power-of-three/
+ * Check leetcode for more approaches
+ * */
+public class PowerOfTen {
+    /*
+     * T : O(logn) 
+     * S : O(logn)
+     * logn is number of digits in integer n
+     * Idea : Any number which is a power of 10 will always looks 
+     * like starting with 1 followed by zero or more 0 . 
+     * */
+    /*
+    public boolean isPowerOfTen(long n) {
+        String s = Long.toString(n);
+        boolean found = s.matches("^10*$");
+        return found;
+    }
+    */
+    
+    
+    /*
+     * T : O(logn) , depends on implementation of Math.log10
+     * S : O(1)
+     * */
+    public boolean isPowerOfTen(long n) {
+        double exp = Math.log10(n);
+        if(exp == (int)exp) {
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    @Test
+    public void testIsPowerOfTen() {
+        PowerOfTen pt = new PowerOfTen();
+        
+        assertTrue("expected true for 1", pt.isPowerOfTen(1));
+        assertTrue("expected true for 10", pt.isPowerOfTen(10));
+        assertTrue("expected true for 100", pt.isPowerOfTen(100));
+        assertTrue("expected true for 10000000000000", pt.isPowerOfTen(10000000000000L));
+        
+        assertFalse("expected false for 2", pt.isPowerOfTen(2));
+        assertFalse("expected false for 3", pt.isPowerOfTen(3));
+        assertFalse("expected false for 1001", pt.isPowerOfTen(1001));
+    }
+}
