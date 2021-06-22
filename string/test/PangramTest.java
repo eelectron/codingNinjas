949a71b50408d2c972a950448d0e3c4e9f498f0f
Prashant Singh <psonlinux@gmail.com>
2021-06-22 15:30:06 +0530

    pangram

--- string/test/PangramTest.java
+++ string/test/PangramTest.java
@@ -14,5 +14,8 @@ public class PangramTest {
         assertEquals("Expected \"abcdfgijkmnpqrstuvwxyz\" ", "abcdfgijkmnpqrstuvwxyz", pg.getAllMissingCharacter("hello"));
         assertEquals("Expected empty string", "", pg.getAllMissingCharacter("abcdefghijklmnopqrstuvwxyz"));
         assertEquals("Expected all letters", "abcdefghijklmnopqrstuvwxyz", pg.getAllMissingCharacter(""));
+        assertEquals("abdhijnpquvxyz", pg.getAllMissingCharacter("welcome to geeksforgeeks"));
+        assertEquals("adglvyz", pg.getAllMissingCharacter("The quick brown fox jumps"));
+        assertEquals("bdfjkpquvxyz", pg.getAllMissingCharacter(" Writing ALGorithmic tests!"));
     }
 }
