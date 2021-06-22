1a20eb4550d7d91be9ea077a6ab28ce29fb91ed9
Prashant Singh <psonlinux@gmail.com>
2021-06-22 16:44:28 +0530

    longest uniform substring

---
+++ string/src/LongestUniformSubstring.java
@@ -0,0 +1,36 @@
+package string.src;
+
+public class LongestUniformSubstring {
+    public int[] longestUniformSubstring(String input) {
+        int longestStart = -1;
+        int longestLength = 0;
+        if(input == null || input.length() == 0){
+          return new int[]{ longestStart, longestLength };
+        }
+        
+        // todo: implement the longestUniformSubstring logic
+        int n = input.length();
+        
+        int ch         = -1;
+        int count       = 0;
+        int charIndex   = -1;
+        for(int i = 0; i < n; i++){
+          if(input.charAt(i) == ch){
+            count += 1;
+          }
+          else{
+              // reset the count and char
+              ch = input.charAt(i);
+              count = 1;
+              charIndex = i;
+          }
+          
+          // possible candidate for answer
+          if(count > longestLength){
+            longestLength = count;
+            longestStart = charIndex;
+          }
+        }
+        return new int[]{longestStart, longestLength};
+    }
+}
